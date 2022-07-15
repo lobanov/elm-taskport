599905d390278d93b17e95c0ad5e5c114a9d2096
Igor Lobanov <1387948+lobanov@users.noreply.github.com>
2022-07-15 09:59:14 +0000

    exporting TaskPort for browser and node

--- js/taskport.js
+++ js/taskport.js
@@ -83,18 +83,13 @@ function register(name, fn) {
 
 
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-      // AMD. Register as an anonymous module.
-      define([], factory);
-  } else if (typeof module === 'object' && module.exports) {
-      // Node. Does not work with strict CommonJS, but
-      // only CommonJS-like environments that support module.exports,
-      // like Node.
+  if (typeof module === 'object' && module.exports) {
+      // Export for CommonJS-like environments like Node
       module.exports = factory();
   } else {
-      // Browser globals (root is window)
-      root.returnExports = factory();
-}
+      // Publish ourselves in browser globals (root is window)
+      root.TaskPort = factory();
+  }
 }(typeof self !== 'undefined' ? self : this, function () {
 
   return {
