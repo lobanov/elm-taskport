a6580c1721ab4a446eb817f09fa9b7c9c72c0af4
Igor Lobanov <1387948+lobanov@users.noreply.github.com>
2022-07-25 15:18:06 +0000

    switched to webpack to build TaskPort library
    this allows more modular code that's easier to test

---
+++ js/url.js
@@ -0,0 +1,20 @@
+export const URL_PREFIX = "elmtaskport://"
+
+/**
+ * Parses given url string and extracts information useful for TaskPort interop.
+ * for the default namespace: elmtaskport:///functionName?v=MAJOR.MINOR.PATCH
+ * for a specific namespace: elmtaskport://author-name/package-name/functionName?v=MAJOR.MINOR.PATCH&nsv=NAMESPACE_VERSION
+ * 
+ * @param {string} url an url as a string
+ * @return {{apiVersion: string, functionName: string, namespaceName: string, namespaceVersion: string} | undefined}
+ * components of the URL or undefined if could not parse
+ */
+export function parseUrl(url) {
+  const m = url.match(/^elmtaskport:\/\/([\w-]+\/[\w-]+)?\/([\w]+)\?v=(\d\.\d\.\d)(?:&nsv=([\w.-]+))?$/);
+  if (m === undefined) {
+    return;
+  }
+
+  const [_, namespaceName, functionName, apiVersion, namespaceVersion] = m;
+  return { namespaceName, namespaceVersion, apiVersion, functionName };
+}
